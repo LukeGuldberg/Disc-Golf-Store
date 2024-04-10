a8178b7cb1386ffb9526f59cf6ceb812b9fc7aad
August Gresens <gresens.august@gmail.com>
2024-04-10 13:20:32 -0500

    Created all beginning files for Vue

---
+++ frontend/disc-golf-store/src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container>
+    <h1>Welcome to the Disc Golf Store</h1>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+}
+</script>
+
+
+<style scoped>
+
+</style>
